053d7306fbc6cbcf8614a48b39f8e541992a7306
Muthaiah26 <pandimuthaiah2006@gmail.com>
2025-12-28 18:31:05 +0530

    socket balancer updated

--- src/utils/socketBalancer.js
+++ src/utils/socketBalancer.js
@@ -1,7 +1,7 @@
 
 function getSocketEndpoint(busNo){
     console.log("Bus number received for socket endpoint:", busNo);
-  const i=Math.floor(parseInt(busNo)/20)%4;
+  const i=Math.floor(parseInt(busNo)/25)%4;
   console.log("Calculated index for load balancing:", i);
   if(i==0){//from 0-19
     return `${process.env.REACT_APP_BACKEND_ENDPOINT1.slice(8)}`;// except https://
